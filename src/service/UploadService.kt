95507615e9f695631c2bc8f440006b5ac4a9f2d1
MartinWienc <32913968+MartinWienc@users.noreply.github.com>
2020-10-12 17:42:50 -0400

    changed download impl to inputstream

--- src/service/UploadService.kt
+++ src/service/UploadService.kt
@@ -6,11 +6,15 @@ import io.ktor.client.*
 import io.ktor.client.request.*
 import model.DathostServerInfo
 import net.dv8tion.jda.api.JDA
+import okhttp3.OkHttpClient
+import okhttp3.Request
+import okhttp3.Response
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
-import java.io.ByteArrayInputStream
 import java.time.MonthDay
 import java.time.Year
+import java.util.*
+import java.util.concurrent.TimeUnit
 
 
 class UploadService {
@@ -20,24 +24,47 @@ class UploadService {
         private var dropboxAppName = System.getenv("dropbox_app_name")
         private var discordTextChannelId: Long = System.getenv("discord_textchannel_id").toLong()
         private var config: DbxRequestConfig = DbxRequestConfig.newBuilder("dropbox/$dropboxAppName").build()
+        val auth64String = "Basic " + Base64.getEncoder()
+            .encodeToString(
+                "${System.getenv("dathost_username")}:${System.getenv("dathost_password")}"
+                    .toByteArray()
+            )
         private lateinit var dropboxClient: DbxClientV2
+        private val httpClient = OkHttpClient.Builder()
+            .readTimeout(10, TimeUnit.MINUTES)
+            .writeTimeout(10, TimeUnit.MINUTES)
+            .callTimeout(5, TimeUnit.MINUTES)
+            .build()
     }
 
     suspend fun uploadDemo(filename: String, gameServerId: String, client: HttpClient, jda: JDA) {
         val demoFileUrl = "https://dathost.net/api/0.1/game-servers/$gameServerId/files/$filename.dem"
         val serverListUrl = "https://dathost.net/api/0.1/game-servers"
         val serverList: List<DathostServerInfo> = client.get(serverListUrl)
-        val demoFile: ByteArray = client.get(demoFileUrl)
         val map = serverList
             .filter { it.id == gameServerId }
             .map { it.csgo_settings?.mapgroup_start_map }
             .firstOrNull() ?: "Unknown Map"
-        dropboxClient = DbxClientV2(config, dropboxToken)
         val uploadPath =
             "/${Year.now()}-${MonthDay.now().month.value}-${MonthDay.now().dayOfMonth}_pug_${map}_$filename.dem"
-        dropboxClient.files().uploadBuilder(uploadPath).uploadAndFinish(ByteArrayInputStream(demoFile))
+        getGameServerFile(demoFileUrl).use { response ->
+            response.body?.byteStream().use { `in` ->
+                dropboxClient.files().uploadBuilder(uploadPath)
+                    .uploadAndFinish(`in`)
+                log.info("Uploaded $uploadPath successfully")
+            }
+        }
+        dropboxClient = DbxClientV2(config, dropboxToken)
         val shareLink = dropboxClient.sharing().createSharedLinkWithSettings(uploadPath)
         val channel = jda.getTextChannelById(discordTextChannelId)
         channel?.sendMessage("New `.dem` replay files are available: \n$map - ${shareLink.url}")?.queue()
+    }
+    private fun getGameServerFile(demoFileUrl: String): Response {
+        val getFileRequest = Request.Builder()
+            .url(demoFileUrl)
+            .get()
+            .header("Authorization", auth64String)
+            .build()
+        return httpClient.newCall(getFileRequest).execute()
     }
 }
