cef2af9d19b29c465bece35a8ef652e2e49b3aab
MartinWienc <32913968+MartinWienc@users.noreply.github.com>
2020-10-14 17:00:58 -0400

    removed embedded server call

--- src/Application.kt
+++ src/Application.kt
@@ -14,7 +14,6 @@ import io.ktor.jackson.*
 import io.ktor.request.*
 import io.ktor.response.*
 import io.ktor.routing.*
-import io.ktor.server.engine.*
 import io.ktor.server.netty.*
 import model.DathostServerInfo
 import model.Match
@@ -29,76 +28,73 @@ fun main(args: Array<String>): Unit = EngineMain.main(args)
 @Suppress("unused") // Referenced in application.conf
 @kotlin.jvm.JvmOverloads
 fun Application.module(testing: Boolean = false) {
-    val port = System.getenv("PORT")?.toInt() ?: 8080
-    embeddedServer(Netty, port) {
-        install(CORS) {
-            method(HttpMethod.Post)
-            method(HttpMethod.Get)
+    install(CORS) {
+        method(HttpMethod.Post)
+        method(HttpMethod.Get)
 //            host("dathost.net", listOf("https"), emptyList())
-            anyHost()
-        }
+        anyHost()
+    }
 
-        install(ContentNegotiation) {
-            jackson {
-                enable(SerializationFeature.INDENT_OUTPUT)
-            }
+    install(ContentNegotiation) {
+        jackson {
+            enable(SerializationFeature.INDENT_OUTPUT)
         }
-        val auth64String = "Basic " + Base64.getEncoder()
-            .encodeToString(
-                "${System.getenv("dathost_username")}:${System.getenv("dathost_password")}"
-                    .toByteArray()
-            )
-        val jda = JDABuilder
-            .create(
-                System.getenv("discord_bot_token"),
-                EnumSet.noneOf(GatewayIntent::class.java)
-            )
-            .setMemberCachePolicy(MemberCachePolicy.NONE)
-            .disableCache(CacheFlag.ACTIVITY)
-            .disableCache(CacheFlag.CLIENT_STATUS)
-            .disableCache(CacheFlag.EMOTE)
-            .disableCache(CacheFlag.VOICE_STATE)
-            .disableCache(CacheFlag.MEMBER_OVERRIDES)
-            .build()
-        val client = HttpClient(Apache) {
-            engine {
-                followRedirects = true
-            }
-            install(JsonFeature) {
-                serializer = GsonSerializer()
-            }
-            defaultRequest {
-                header("Authorization", auth64String)
-                accept(ContentType.Application.Json)
-            }
+    }
+    val auth64String = "Basic " + Base64.getEncoder()
+        .encodeToString(
+            "${System.getenv("dathost_username")}:${System.getenv("dathost_password")}"
+                .toByteArray()
+        )
+    val jda = JDABuilder
+        .create(
+            System.getenv("discord_bot_token"),
+            EnumSet.noneOf(GatewayIntent::class.java)
+        )
+        .setMemberCachePolicy(MemberCachePolicy.NONE)
+        .disableCache(CacheFlag.ACTIVITY)
+        .disableCache(CacheFlag.CLIENT_STATUS)
+        .disableCache(CacheFlag.EMOTE)
+        .disableCache(CacheFlag.VOICE_STATE)
+        .disableCache(CacheFlag.MEMBER_OVERRIDES)
+        .build()
+    val client = HttpClient(Apache) {
+        engine {
+            followRedirects = true
         }
-        routing {
-            route("/api") {
-                post("/match-end") {
-                    val match = call.receive<Match>()
-                    UploadService().uploadDemo(match.id, match.game_server_id, client, jda)
-                    call.respond(HttpStatusCode.OK)
-                }
-                route("/server") {
-                    get("/online") {
-                        val serverId = call.parameters["serverid"]
-                        val serverListUrl = "https://dathost.net/api/0.1/game-servers"
-                        val serverList: List<DathostServerInfo> = client.get(serverListUrl)
-                        val serverOn = serverList
-                            .filter { it.id == serverId }
-                            .map { it.on }
-                            .firstOrNull()
-                        serverOn?.let {
-                            call.respond(it)
-                            return@get
-                        }
-                        call.respond(HttpStatusCode.NoContent)
+        install(JsonFeature) {
+            serializer = GsonSerializer()
+        }
+        defaultRequest {
+            header("Authorization", auth64String)
+            accept(ContentType.Application.Json)
+        }
+    }
+    routing {
+        route("/api") {
+            post("/match-end") {
+                val match = call.receive<Match>()
+                UploadService().uploadDemo(match.id, match.game_server_id, client, jda)
+                call.respond(HttpStatusCode.OK)
+            }
+            route("/server") {
+                get("/online") {
+                    val serverId = call.parameters["serverid"]
+                    val serverListUrl = "https://dathost.net/api/0.1/game-servers"
+                    val serverList: List<DathostServerInfo> = client.get(serverListUrl)
+                    val serverOn = serverList
+                        .filter { it.id == serverId }
+                        .map { it.on }
+                        .firstOrNull()
+                    serverOn?.let {
+                        call.respond(it)
+                        return@get
                     }
+                    call.respond(HttpStatusCode.NoContent)
                 }
             }
-
         }
-    }.start(wait = true)
+
+    }
 }
 
 
--- test/ApplicationTest.kt
+++ test/ApplicationTest.kt
@@ -1,18 +1,15 @@
 package com.martige
 
-import io.ktor.application.*
 import io.ktor.http.*
 import io.ktor.server.testing.*
-import kotlin.test.Ignore
 import kotlin.test.Test
 import kotlin.test.assertEquals
 import kotlin.test.assertNotNull
 
 class ApplicationTest {
     @Test
-    @Ignore
     fun testRoot() {
-        withTestApplication(Application::module) {
+        withTestApplication({ module(testing = true) }) {
             handleRequest(HttpMethod.Get, "/api/server/online?serverid=5f79568d3fb31f923b109df9").apply {
                 assertEquals(HttpStatusCode.OK, response.status())
                 assertNotNull(response.content)
