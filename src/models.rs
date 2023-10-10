f78d03209c531b65063b108a9ea9f46a0f939429
Martin <wienc.martin@gmail.com>
2023-10-10 18:30:07 -0400

    impl match end webhook

--- src/models.rs
+++ src/models.rs
@@ -1,14 +1,100 @@
 use derive_more::{AsRef, Deref, Display, From, Into};
 use serde::{Deserialize, Serialize};
-#[derive(Debug, From, Into, Deref, AsRef, Display, Serialize, Deserialize)]
+use serde_aux::prelude::*;
+use sqlx::types::time::OffsetDateTime;
+#[derive(Debug, Clone, From, Into, Deref, AsRef, Display, Serialize, Deserialize)]
 #[repr(transparent)]
 pub struct ServerId(pub(crate) String);
-#[derive(Debug, Deserialize, Serialize)]
-pub struct DatHostMatch {
-    pub id: String,
+#[derive(Clone, Debug, From, Into, Deref, AsRef, Display, Serialize, Deserialize)]
+#[repr(transparent)]
+pub struct DathostMatchId(pub(crate) String);
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub struct DathostMatchEnd {
+    pub id: DathostMatchId,
     #[serde(rename = "game_server_id")]
     pub server_id: ServerId,
-    pub match_series_id: Option<String>,
-    pub map: Option<String>,
+    pub team1: Team,
+    pub team2: Team,
+    pub players: Vec<Player>,
+    pub settings: Settings,
+    pub rounds_played: i32,
     pub finished: bool,
+    pub cancel_reason: Option<String>,
+}
+
+#[derive(Default, Debug, Clone, PartialEq, Serialize, Deserialize)]
+pub struct Team {
+    pub name: String,
+    pub stats: MatchStats,
+}
+
+#[derive(Default, Debug, Clone, PartialEq, Serialize, Deserialize)]
+pub struct MatchStats {
+    pub score: i32,
+}
+
+#[derive(Default, Debug, Clone, PartialEq, Serialize, Deserialize)]
+pub struct Player {
+    pub match_id: String,
+    #[serde(deserialize_with = "deserialize_number_from_string")]
+    pub steam_id_64: i64,
+    pub team: String,
+    pub connected: bool,
+    pub kicked: bool,
+    pub stats: PlayerStats,
+}
+
+#[derive(Default, Debug, Clone, PartialEq, Serialize, Deserialize)]
+pub struct PlayerStats {
+    pub kills: i32,
+    pub assists: i32,
+    pub deaths: i32,
+    pub mvps: i32,
+    pub score: i32,
+    #[serde(rename = "2ks")]
+    pub n2ks: i32,
+    #[serde(rename = "3ks")]
+    pub n3ks: i32,
+    #[serde(rename = "4ks")]
+    pub n4ks: i32,
+    #[serde(rename = "5ks")]
+    pub n5ks: i32,
+    pub kills_with_headshot: i32,
+    pub kills_with_pistol: i32,
+    pub kills_with_sniper: i32,
+    pub damage_dealt: i32,
+    pub entry_attempts: i32,
+    pub entry_successes: i32,
+    pub flashes_thrown: i32,
+    pub flashes_successful: i32,
+    pub flashes_enemies_blinded: i32,
+    pub utility_thrown: i32,
+    pub utility_damage: i32,
+    #[serde(rename = "1vX_attempts")]
+    pub n1v_x_attempts: i32,
+    #[serde(rename = "1vX_wins")]
+    pub n1v_x_wins: i32,
+}
+
+#[derive(Default, Debug, Clone, PartialEq, Serialize, Deserialize)]
+pub struct Settings {
+    pub map: String,
+    pub connect_time: i64,
+    pub match_begin_countdown: i64,
+}
+
+#[derive(Debug, Clone)]
+pub struct DathostMatch {
+    pub id: i32,
+    pub map: String,
+    pub team1_score: i32,
+    pub team2_score: i32,
+    pub team1_name: String,
+    pub team2_name: String,
+    pub completed_at: OffsetDateTime,
+}
+
+#[derive(Deserialize)]
+pub struct MatchEndParams {
+    pub wait_for_gotv: Option<bool>,
 }
--- src/routes/webhooks.rs
+++ src/routes/webhooks.rs
@@ -1,7 +1,8 @@
+use crate::db::{create_match, create_match_stats};
 use crate::errors::Error;
-use crate::models::DatHostMatch;
+use crate::models::{DathostMatchEnd, MatchEndParams};
 use crate::AppState;
-use axum::extract::State;
+use axum::extract::{Query, State};
 use axum::response::IntoResponse;
 use axum::routing::post;
 use axum::{Json, Router};
@@ -16,21 +17,42 @@ pub fn webhook_routes() -> Router<AppState> {
 
 pub async fn match_end(
     state: State<AppState>,
-    dathost_match: Json<DatHostMatch>,
+    query: Query<MatchEndParams>,
+    dathost_match: Json<DathostMatchEnd>,
 ) -> impl IntoResponse {
-    let tv_delay = env::var("TV_DELAY").unwrap_or("105".to_string());
-    sleep(Duration::from_secs(tv_delay.parse::<u64>().unwrap() + 30)).await;
+    if let Some(reason) = &dathost_match.cancel_reason {
+        println!("cancel reason: {}", reason);
+        return Ok(StatusCode::OK);
+    }
+    let created_match = create_match(&state.db, &dathost_match.0).await?;
+    create_match_stats(&state.db, &dathost_match.0, created_match.id).await?;
+    let delay = match query.wait_for_gotv {
+        Some(b) => b,
+        None => false,
+    };
+    if delay {
+        let tv_delay = env::var("TV_DELAY")
+            .unwrap_or("105".to_string())
+            .parse::<u64>()
+            .unwrap()
+            + 30;
+        println!("sleeping for {} sec", tv_delay);
+        sleep(Duration::from_secs(tv_delay)).await;
+    }
     let stop_status = state.dathost.stop_server(&dathost_match.server_id).await?;
-    if stop_status.as_u16() != 500 {
+    if stop_status.as_u16() != 200 {
         return Err(Error::StopServerError);
     }
     let path = format!("{}.dem", dathost_match.id);
+    println!("fetching demo file '{}'", path);
     let demo = state
         .dathost
         .get_file(&dathost_match.server_id, &path)
         .await?;
-
-    if state.bucket.put_object(path, &demo).await?.status_code() != 200 {
+    println!("uploading demo to s3");
+    let s3_status = state.bucket.put_object(path, &demo).await?.status_code();
+    if s3_status != 200 {
+        eprintln!("s3 error: {}", s3_status);
         return Err(Error::DemoUploadError);
     }
     Ok(StatusCode::OK)
